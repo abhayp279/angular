557bc6e4100cb41cd546dc78439f3d4536b5e1a7
abhayp279 <abhay.poduri@halliburton.com>
2018-06-11 14:11:57 -0500

    Observables

--- conFusion/src/app/about/about.component.ts
+++ conFusion/src/app/about/about.component.ts
@@ -30,7 +30,7 @@ export class AboutComponent implements OnInit {
   ngOnInit() {
     let id = +this.route.snapshot.params['id'];
     //this.leaders = this.leaderservice.getLeaders();
-    this.leaderservice.getLeaders().then(leaders => this.leaders = leaders);
+    this.leaderservice.getLeaders().subscribe(leaders => this.leaders = leaders);
    
   }
 
